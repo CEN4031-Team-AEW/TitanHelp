bdca0caf58bf17404645fa320ddca9a7b4e281ab
epham98 <lacpham0103@gmail.com>
2022-10-01 20:48:38 -0400

    added customer/ticket clases, will act as objects

---
+++ TitanHelp/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TitanHelp.Models
+{
+    public class Customer
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
---
+++ TitanHelp/Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TitanHelp.Models
+{
+    public class Ticket
+    {
+        public DateTime TicketDate { get; set; }
+        public string Description { get; set; }
+        public bool Resolved { get; set; }
+    }
+}
